df3d1fa519a46acab5798549d4cfb15d608bda3a
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-29 21:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ function yourAnimal() {
   return animal;
 }
 
+const two = 2
+
 function add2(n) {
   return n + two
 
-  // Feel free to move things around!
-  const two = 2
-}
 
 var funkyFunction = function() {
   return function() {
